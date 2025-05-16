eb9d62eb57deaa260937c90478bad72f2da48c77
Arturo Resendiz <resendizdeoz@hotmail.com>
2025-05-15 22:40:33 -0600

    is invalid

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,11 +7,16 @@
     <div class="container p-5 mt-1">
         <div class="row mt-5 justify-content-center">
             <div class="col-sm-12 col-md-9 col-lg-4 mt-1 bg-white py-5 border border-5 sombra">
-                <div class="row">
+                <div class="row justify-content-center">
                     <div class="col-12 text-center">
                         <img src="img/user.gif" class="img-fluid w-50" alt="">
                         <h3 class="mt-2">Inicio de sesión</h3>
-                        <span class="text-danger">{{$errors->first()}}</span> 
+                    </div>
+                    <div class="col-lg-9 text-center">
+                        @if ($errors->first())
+                        <div class="h6"> <i class="fa fa-info-circle text-danger "></i>  {{$errors->first()}}  </div> 
+                        
+                    @endif
                     </div>
                 </div>
 
@@ -25,7 +30,7 @@
                             <div class="input-group-icon">
                                 <span class="input-group-text"> <i class="fa fa-user p-1"></i> </span>
                             </div>
-                            <input type="email" name="email" class="form-control login" value="{{old('email')}}" aria-describedby="input-group-icon" placeholder="Usuario" autofocus >
+                            <input type="email" name="email" class="form-control login {{$errors->first() ? 'is-invalid' : ''}}" value="{{old('email')}}" aria-describedby="input-group-icon" placeholder="Usuario" autofocus >
                         </div>
                     </div>
                 </div>
@@ -36,7 +41,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text"> <i class="fa fa-lock p-1"></i> </span>
                             </div>
-                            <input type="password" name="password" class="form-control form-control-sm login" value="{{old('password')}}"  placeholder="Contraseña" >
+                            <input type="password" name="password" class="form-control form-control-sm login {{$errors->first() ? 'is-invalid' : ''}}" value="{{old('password')}}"  placeholder="Contraseña" >
                         </div>
                     </div>
                 </div>
